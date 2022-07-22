2a08b1c7cbedf21b222110a6181618a8fb22fd19
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-22 09:36:32 -0700

    Created Menu
    Created the page menu and functionaility, and started the home page

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector(".menu-btn__burger");
+
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector('.menu-nav');
+
+menuBtn.addEventListener('click', function(){
+    hamburger.classList.toggle("open");
+    nav.classList.toggle("open");
+    menuNav.classList.toggle("open");
+} );
