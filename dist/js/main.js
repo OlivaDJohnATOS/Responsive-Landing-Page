3e7bfe9fa1a8c948c00451c2a7f033fa7054bc4b
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-22 12:03:06 -0700

    Added Services section

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,8 +4,20 @@ const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector('.menu-nav');
 
+const menuItems = document.getElementsByClassName('menu-nav__item');
+
+let state = false;
+
 menuBtn.addEventListener('click', function(){
     hamburger.classList.toggle("open");
     nav.classList.toggle("open");
     menuNav.classList.toggle("open");
-} );
+});
+
+for (let i=0; i< menuItems.length; i++){
+    menuItems[i].addEventListener('click', function(){
+        hamburger.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+    });
+}
